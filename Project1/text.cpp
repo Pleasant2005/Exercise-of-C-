c7490093991b42a5249ee54ebc2ada68f9746eaf
Pleasant2005 <yinjunyi2005@outlook.com>
2024-04-20 22:30:16 +0800

    修改了Project1 学习内容为用一维数组给同学存储8门课程的成绩

--- Project1/text.cpp
+++ Project1/text.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
-main()
+int main(void) 
 {
 	int i, a[8];
-	printf()
+	printf("请输入你8门课程的成绩：\n");
+	for (i = 0;i < 8;i++)
+		scanf_s("%d", &a[i]);
+	for (i = 0;i < 8;i++)
+		printf("%d\n", a[i]);
 }
